f22409e2adbdff7499c6b6e514c103bac0187fac
mononobealice <273421673@qq.com>
2025-01-13 16:04:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,22 @@
 
 import ncatpy
 from ncatpy import logging
-from ncatpy.message import GroupMessage
+from ncatpy.message import GroupMessage, PrivateMessage
 
 _log = logging.get_logger()
+
 
 class MyClient(ncatpy.Client):
     async def on_group_message(self, message: GroupMessage):
         _log.info(f"收到群消息，ID: {message.user_id}，{message.message}")
-        if message.message.text.text == "你好":
+        if message.message.text and message.message.text.text == "你好":
             # 通过http发送消息
             t = await message.add_text("你好,o").reply()
             _log.info(t)
 
+    async def on_private_message(self, message: PrivateMessage):
+        _log.info(f"收到私聊消息，ID: {message.user_id}，{message.message}")
 
-    # async def on_private_message(self, message: PrivateMessage):
-    #     _log.info(f"收到私聊消息，ID: {message.message.text.text}")
-    #     if message.message.text.text == "你好":
-    #         t = await self._api.send_msg(user_id=message.user_id, text="你好,o")
-    #         _log.info(t)
 
 if __name__ == "__main__":
     # 1. 通过预设置的类型，设置需要监听的事件通道
